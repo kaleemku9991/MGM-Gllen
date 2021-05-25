3f04e2f22db3d00ce0e130a42cbb685e406de121
kaleemku9991 <kaleemku9991@gmail.com>
2021-05-25 17:23:31 +0500

    this

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,7 +1,15 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-  '/'
-
+  '/index.html',
+  '/js/main.js',
+  '/css/style.css',
+  '/fabrications.html',
+  '/components.html',
+  '/extrusion.html',
+  '/rollForm.html',
+  '/tubing.html',
+  '/js/products.js',
+  '/js/fabrications.js',
 ];
 
 self.addEventListener('install', function(event) {
