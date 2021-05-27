49c3e0bc594f55ff5db1dbf4999af56cb103c125
kaleemku9991 <kaleemku9991@gmail.com>
2021-05-27 14:27:51 +0500

    this

--- js/main.js
+++ js/main.js
@@ -95,26 +95,26 @@ function currentSlide(n) {
   myTimer = setInterval(function () { plusSlides(n + 1) }, 4000);
   showSlides(slideIndex = n);
 }
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
 
-window.resize(function(){
-  var wid = window.width();
-  if(wid < 768){
+// window.resize(function(){
+//   var wid = window.width();
+//   if(wid < 768){
     
-    // Close the dropdown menu if the user clicks outside of it
-    window.onclick = function(event) {
-      if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-          var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-          }
-        }
-      }
-    }
-  }
-});
+//     // Close the dropdown menu if the user clicks outside of it
+//     window.onclick = function(event) {
+//       if (!event.target.matches('.dropbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//           var openDropdown = dropdowns[i];
+//           if (openDropdown.classList.contains('show')) {
+//             openDropdown.classList.remove('show');
+//           }
+//         }
+//       }
+//     }
+//   }
+// });
